d7c072cc5a2b4fa21a7a7527cdcb3c845926e7ce
oumarsowbbsy <oo2sowbbsy@gmail.com>
2021-08-26 23:55:10 +0200

    correction retour

--- src/Form/RacheterType.php
+++ src/Form/RacheterType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Racheter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -12,8 +13,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RacheterType extends AbstractType
 {
+
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $choices = [
+            'Oui' => 'Oui',
+            'Non' => 'Non'
+        ];
         $builder
             ->add('marque', TextType::class, [
                 'label' => 'Marque de votre voiture:'
@@ -31,7 +39,12 @@ class RacheterType extends AbstractType
                 'mapped' => false,
                 'required' => false
             ])
-            ->add('enpanne', NumberType::class, [
+
+            ->add('enpanne', ChoiceType::class, [
+                'choices' => $choices,
+                'expanded' => true,
+                'data' => 'Oui',
+                'required' => true,
                 'label' => 'Voiture en panne:'
             ])
             ->add('nom', TextType::class, [
