d68a233692c55991d3b85640816f4001de5a260a
Oumar Ousmane Sow <oumar-ousmane.sow.external@worldline.com>
2021-11-29 13:26:19 +0100

    test

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Mime\Email;
 use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -13,27 +15,29 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request, \Swift_Mailer $mailer)
+    public function index(Request $request, \Swift_Mailer $mailer, MailerInterface $mailerInterface)
     {
         $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
         if($form->isSubmitted()&& $form->isValid()){
             $contact = $form->getData();
-            $message = (new \Swift_Message('Nouveau contact'))
+         //   $message = (new \Swift_Message('Nouveau contact'))
+            $message = (new Email())
                 // On attribue l'expéditeur
-                ->setFrom($form->get('email')->getData())
+                ->from($form->get('email')->getData())
                 // On attribue le destinataire
-                ->setTo('sowoumarousmane@gmail.com')
+                ->to('sowoumarousmane@gmail.com')
+                ->subject('test ok')
                 // On crée le texte avec la vue
-                ->setBody(
+                ->text(
                     $this->renderView(
                         'contact/email.html.twig', compact('contact')
                     ),
                     'text/html'
                 )
             ;
-            $mailer->send($message);
+            $mailerInterface->send($message);
             $this->addFlash('message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.');
         }
         return $this->render('contact/index.html.twig', [
