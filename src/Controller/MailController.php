e2750f9b8ed7b8606cc88f55a81836af09427baa
oumarsowbbsy <oo2sowbbsy@gmail.com>
2022-04-24 17:21:00 +0200

    modif client

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -44,6 +44,7 @@ class MailController extends AbstractController
                 ),
                 'text/html'
             );
+            $mailer->send($message);
             dump($mailer->send($message));
         }
 
